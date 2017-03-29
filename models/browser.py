89a856524ddd4a1145634f209721b594244aecd3
Amos Ng <amosng1@gmail.com>
2017-03-29 21:58:44 +0800

    Added placeholder Browser class

---
+++ models/browser.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+##
+# RedditNewsPostBot Browser Model
+# By Amos (LFlare) Ng
+##
+# Import dependencies
+import sys
+
+from PyQt5 import QtCore
+from PyQt5 import QtGui
+from PyQt5 import QtWebEngineWidgets
+
+
+class Browser():
+    pass

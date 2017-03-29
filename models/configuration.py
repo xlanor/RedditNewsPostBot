075e743de13f2fa39d14bced2242fbaa18a9feac
Amos Ng <amosng1@gmail.com>
2017-03-29 22:50:03 +0800

    Migrated supported_sites out to it's own configuration file

--- models/configuration.py
+++ models/configuration.py
@@ -14,8 +14,10 @@ from models.exceptions import *
 class Configuration():
     configuration = None
 
-    def __init__(self, configuration_file="config.json",
-                 credentials_file="credentials.json"):
+    def __init__(self,
+                 configuration_file="config.json",
+                 credentials_file="credentials.json",
+                 supported_sites_file="supported_sites.json"):
         try:
             configuration = open(configuration_file, "r").read()
             parsed_configuration = re.sub(r"// .*\n", r"\n", configuration)
@@ -24,6 +26,10 @@ class Configuration():
             credentials = open(credentials_file, "r").read()
             parsed_credentials = re.sub(r"// .*\n", r"\n", credentials)
             self.configuration["credentials"] = json.loads(parsed_credentials)
+
+            supported_sites = open(supported_sites_file, "r").read()
+            parsed_supported_sites = re.sub(r"// .*\n", r"\n", supported_sites)
+            self.configuration["supported_sites"] = json.loads(parsed_supported_sites)
         except:
             raise ConfigurationException(
                 "Configuration invalid, please check json syntax!")
